a687f64eeac46f68e9091fed4b8a0fdfded93a47
agusbw <agusbudhiarta81@gmail.com>
2024-05-08 17:21:15 +0800

    fix: add responsive breakpoint

--- src/app/(tools)/summarize/page.tsx
+++ src/app/(tools)/summarize/page.tsx
@@ -14,10 +14,10 @@ function SummarizePage() {
 
   return (
     <div className="flex flex-col gap-8 pt-6 md:h-[77%] md:flex-row">
-      <ScrollArea className="h-full max-h-[100%] overflow-y-auto rounded-lg border shadow-md md:w-1/3">
+      <ScrollArea className="h-full max-h-[100%] overflow-y-auto rounded-lg border shadow-md md:w-1/2 lg:w-1/3">
         <ConfigForm handleSummaryChange={setData} />
       </ScrollArea>
-      <ScrollArea className="h-full max-h-[100%] overflow-y-auto rounded-lg border p-5 shadow-md md:w-2/3">
+      <ScrollArea className="h-full max-h-[100%] overflow-y-auto rounded-lg border p-5 shadow-md md:w-1/2 lg:w-2/3">
         <ResultBox data={data} />
       </ScrollArea>
     </div>
