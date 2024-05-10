a521361fc17c39d767cc12151dda515a2bd532fc
agus_bw <agusbudhiarta81@gmail.com>
2024-05-10 12:13:21 +0800

    fix: summarize layout

--- src/app/(tools)/summarize/page.tsx
+++ src/app/(tools)/summarize/page.tsx
@@ -14,10 +14,10 @@ function SummarizePage() {
 
   return (
     <div className="flex flex-col gap-8 overflow-y-auto pt-6 md:flex-row">
-      <ScrollArea className="rounded-lg border shadow-md md:w-1/2 lg:w-1/3">
+      <ScrollArea className="rounded-lg border shadow-md md:max-w-sm md:flex-1">
         <ConfigForm handleSummaryChange={setData} />
       </ScrollArea>
-      <ScrollArea className="rounded-lg border p-5 shadow-md md:w-1/2 lg:w-2/3">
+      <ScrollArea className="rounded-lg border p-5 shadow-md md:flex-1">
         <ResultBox data={data} />
       </ScrollArea>
     </div>
