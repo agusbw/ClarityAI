837bce0f67dcd0b9b87ae834d5921af5d7079981
agus_bw <agusbudhiarta81@gmail.com>
2024-05-10 14:53:48 +0800

    chore: disable image optimizatio

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  // because we're using static export
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
